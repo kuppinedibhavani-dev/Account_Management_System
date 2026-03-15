54199ab675f9d78308ad42e17f96e5c3bcc656dc
kuppinedibhavani_dev <kuppinedibhavani@gmail.com>
2026-03-15 17:58:47 +0530

    Updated auth

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { signup,login } from "../controllers/authController.js";
 const router=express.Router()
 
-router.post("/sign-up",signup)
+router.post("/signup",signup)
 router.post("/login",login)
 
 export default router;

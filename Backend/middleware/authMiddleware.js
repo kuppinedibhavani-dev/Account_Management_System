1f69fe4579a5f638e98c9042f6b9d35389639b4e
kuppinedibhavani_dev <kuppinedibhavani@gmail.com>
2026-03-15 18:26:44 +0530

    Updated

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,16 +1,26 @@
 import jwt from "jsonwebtoken";
 
-export const protect=(req,res,next)=>{
-    const token=req.headers.authorization?.split("")[1]
+export const protect = (req, res, next) => {
 
-    if(!token){
-        return res.status(401).json({message:"Not authorized"})
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1];
+
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = decoded;
+            next();
+
+        } catch (error) {
+            res.status(401).json({ message: "Invalid token" });
+        }
+
+    } else {
+        res.status(401).json({ message: "No token provided" });
     }
-    try{
-        const decoded=jwt.verify(token,process.env.JWT_SECRET)
-        req.user=decoded.id
-        next()
-    }catch(error){
-        res.status(401).json({message:"Invalid token"})
-    }
-}
+};
